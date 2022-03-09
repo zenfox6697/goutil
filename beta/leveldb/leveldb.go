9465fe7c63e55db634b803bff6f3220c3e7e57c6
zenfox <yush2629@163.com>
2022-03-09 11:09:36 +0800

    fix:leveldb

--- beta/leveldb/leveldb.go
+++ beta/leveldb/leveldb.go
@@ -1,4 +1,5 @@
 // beta:leveldb v0.1
+// need rework
 package leveldb
 
 import (
@@ -7,31 +8,51 @@ import (
 	"github.com/syndtr/goleveldb/leveldb"
 )
 
-type level struct {
-	Path string
-	Key  string
-	DB   *leveldb.DB
-}
+// type level struct {
+// 	Path string
+// 	Key  string
+// 	DB   *leveldb.DB
+// }
 
-func NewLevelDB(path, key string) *level {
-	db, err := leveldb.OpenFile(path, nil)
+// // func NewLevelDB(path, key string) *level {
+// // 	db, err := leveldb.OpenFile(path, nil)
+// // 	if err != nil {
+// // 		log.Println(err)
+// // 	}
+// // 	return &level{Path: path, DB: db, Key: key}
+// // }
+
+// var lv level
+
+// func InitLevelDB(path string) {
+// 	db, err := leveldb.OpenFile(path, nil)
+// 	if err != nil {
+// 		log.Println(err)
+// 	}
+// 	lv = level{Path: path, DB: db}
+// }
+
+// func (l *level) PutString(k, v string) {
+// 	err := l.DB.Put([]byte(l.Key+k), []byte(v), nil)
+// 	if err != nil {
+// 		log.Println(err)
+// 	}
+// }
+
+// func (l *level) GetString(k string) string {
+// 	v, err := l.DB.Get([]byte(l.Key+k), nil)
+// 	if err != nil {
+// 		log.Println(err)
+// 	}
+// 	return string(v)
+// }
+
+var Level *leveldb.DB
+
+func InitLevelDB(addr string) {
+	db, err := leveldb.OpenFile(addr, nil)
 	if err != nil {
 		log.Println(err)
 	}
-	return &level{Path: path, DB: db, Key: key}
-}
-
-func (l *level) PutString(k, v string) {
-	err := l.DB.Put([]byte(l.Key+k), []byte(v), nil)
-	if err != nil {
-		log.Println(err)
-	}
-}
-
-func (l *level) GetString(k string) string {
-	v, err := l.DB.Get([]byte(l.Key+k), nil)
-	if err != nil {
-		log.Println(err)
-	}
-	return string(v)
+	Level = db
 }
